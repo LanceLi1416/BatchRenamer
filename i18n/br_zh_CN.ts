f98a3fef065a1603586ea09b01cbd96b49d5ec00
Lance Li <46139702+LanceLi1416@users.noreply.github.com>
2022-09-06 19:20:49 -0400

    Setup translation

---
+++ i18n/br_zh_CN.ts
@@ -0,0 +1,347 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>BatchRenamer</name>
+    <message>
+        <location filename="../Widgets/BatchRenamer.cpp" line="34"/>
+        <source>Batch Renamer</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Widgets/BatchRenamer.cpp" line="43"/>
+        <source>Add a pallet</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Widgets/BatchRenamer.cpp" line="43"/>
+        <source>Remove selected pallet(s)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Widgets/BatchRenamer.cpp" line="44"/>
+        <source>Add files and folders</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Widgets/BatchRenamer.cpp" line="44"/>
+        <source>Remove selected files and folders</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Widgets/BatchRenamer.cpp" line="46"/>
+        <source>Current Name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Widgets/BatchRenamer.cpp" line="46"/>
+        <source>New Name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Widgets/BatchRenamer.cpp" line="78"/>
+        <source>File</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Widgets/BatchRenamer.cpp" line="80"/>
+        <source>Apply Changes</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Widgets/BatchRenamer.cpp" line="81"/>
+        <source>Add Files...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Widgets/BatchRenamer.cpp" line="82"/>
+        <source>Remove Selected Files</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Widgets/BatchRenamer.cpp" line="83"/>
+        <source>Remove All Files</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Widgets/BatchRenamer.cpp" line="84"/>
+        <source>Remove Unaffected Files</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Widgets/BatchRenamer.cpp" line="85"/>
+        <source>New Pallet</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Widgets/BatchRenamer.cpp" line="86"/>
+        <source>Remove Selected Pallets</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Widgets/BatchRenamer.cpp" line="87"/>
+        <source>Remove All Pallets</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Widgets/BatchRenamer.cpp" line="88"/>
+        <source>Close Window</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Widgets/BatchRenamer.cpp" line="123"/>
+        <source>Edit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Widgets/BatchRenamer.cpp" line="126"/>
+        <source>View</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Widgets/BatchRenamer.cpp" line="129"/>
+        <source>Window</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Widgets/BatchRenamer.cpp" line="131"/>
+        <source>Minimize</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Widgets/BatchRenamer.cpp" line="132"/>
+        <source>Zoom</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Widgets/BatchRenamer.cpp" line="251"/>
+        <source>Select Files to Rename</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>brFindAndReplace</name>
+    <message>
+        <location filename="../Widgets/brFindAndReplace.cpp" line="11"/>
+        <source>Find &amp; Replace</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Widgets/brFindAndReplace.cpp" line="12"/>
+        <source>Number</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Widgets/brFindAndReplace.cpp" line="13"/>
+        <source>Set Extension</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Widgets/brFindAndReplace.cpp" line="14"/>
+        <source>Remove Text</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Widgets/brFindAndReplace.cpp" line="16"/>
+        <source>Apply To:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Widgets/brFindAndReplace.cpp" line="18"/>
+        <source>Name Only</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Widgets/brFindAndReplace.cpp" line="19"/>
+        <source>Extension Only</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Widgets/brFindAndReplace.cpp" line="20"/>
+        <source>Name and Extension</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Widgets/brFindAndReplace.cpp" line="22"/>
+        <source>Find:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Widgets/brFindAndReplace.cpp" line="25"/>
+        <source>Replace:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Widgets/brFindAndReplace.cpp" line="28"/>
+        <source>Case Sensitive</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Widgets/brFindAndReplace.cpp" line="29"/>
+        <source>First Match Only</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>brNumber</name>
+    <message>
+        <location filename="../Widgets/brNumber.cpp" line="11"/>
+        <source>Find &amp; Replace</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Widgets/brNumber.cpp" line="12"/>
+        <source>Number</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Widgets/brNumber.cpp" line="13"/>
+        <source>Set Extension</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Widgets/brNumber.cpp" line="14"/>
+        <source>Remove Text</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Widgets/brNumber.cpp" line="17"/>
+        <source>Apply To:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Widgets/brNumber.cpp" line="19"/>
+        <source>Name Only</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Widgets/brNumber.cpp" line="20"/>
+        <source>Extension Only</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Widgets/brNumber.cpp" line="21"/>
+        <source>Name and Extension</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Widgets/brNumber.cpp" line="23"/>
+        <source>Digits:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Widgets/brNumber.cpp" line="27"/>
+        <source>Start value:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Widgets/brNumber.cpp" line="31"/>
+        <source>Step value:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Widgets/brNumber.cpp" line="35"/>
+        <source>Insert text:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Widgets/brNumber.cpp" line="38"/>
+        <source>Format:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Widgets/brNumber.cpp" line="40"/>
+        <source>Original - text - number</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Widgets/brNumber.cpp" line="41"/>
+        <source>Number - text - original</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Widgets/brNumber.cpp" line="42"/>
+        <source>Text - number</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Widgets/brNumber.cpp" line="43"/>
+        <source>Number - text</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Widgets/brNumber.cpp" line="45"/>
+        <source>Continue Numbering</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>brRemoveText</name>
+    <message>
+        <location filename="../Widgets/brRemoveText.cpp" line="11"/>
+        <source>Find &amp; Replace</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Widgets/brRemoveText.cpp" line="12"/>
+        <source>Number</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Widgets/brRemoveText.cpp" line="13"/>
+        <source>Set Extension</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Widgets/brRemoveText.cpp" line="14"/>
+        <source>Remove Text</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Widgets/brRemoveText.cpp" line="17"/>
+        <source>Remove:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Widgets/brRemoveText.cpp" line="20"/>
+        <source>First Match Only</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>brSetExtension</name>
+    <message>
+        <location filename="../Widgets/brSetExtension.cpp" line="11"/>
+        <source>Find &amp; Replace</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Widgets/brSetExtension.cpp" line="12"/>
+        <source>Number</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Widgets/brSetExtension.cpp" line="13"/>
+        <source>Set Extension</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Widgets/brSetExtension.cpp" line="14"/>
+        <source>Remove Text</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Widgets/brSetExtension.cpp" line="17"/>
+        <source>Extension:</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>brToolBar</name>
+    <message>
+        <location filename="../Widgets/brToolBar.cpp" line="8"/>
+        <source>RUN</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
--- main.cpp
+++ main.cpp
@@ -1,9 +1,23 @@
 #include <QApplication>
+#include <QTranslator>
 
 #include "Widgets/BatchRenamer.h"
 
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
+
+    QTranslator translator;
+    const QStringList uiLanguages = QLocale::system().uiLanguages();
+    qDebug() << QLocale();
+    for (const QString &locale: uiLanguages) {
+        const QString baseName = "br_" + QLocale(locale).name();
+        qDebug() << baseName;
+        if (translator.load(":/i18n/i18n/" + baseName)) {
+            QApplication::installTranslator(&translator);
+            break;
+        }
+    }
+
     BatchRenamer w;
     w.show();
     return QApplication::exec();
